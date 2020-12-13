dec63898f1154ea4096a266634b667c20845203b
roman.vlasenko <roman.vlasenko@ev-box.com>
2020-12-13 18:15:31 +0100

    Level 0 - The Swamp of POX

---
+++ src/test/kotlin/com/donerpoint/shawarma/ShawarmaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.donerpoint.shawarma
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShawarmaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
